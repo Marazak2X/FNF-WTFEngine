899e0296ae20f90202fa9b0a3a41ca31aa54bec8
Marazak2X <123882434+Marazak2X@users.noreply.github.com>
2023-02-26 15:15:50 +0900

    Add files via upload

---
+++ docs/script.hx
@@ -0,0 +1,52 @@
+function onCreate()
+{
+  //create script
+}
+
+function onCreatePost()
+{
+   //create post
+}
+
+function onUpdate()
+{
+   //update
+}
+
+function onUpdatePost()
+{
+  //update post
+}
+
+function onStepHit()
+{
+  //step hit
+}
+
+function onSongStart()
+{
+   //song start
+}
+
+
+function onBeatHit()
+{
+   //beat hit
+}
+
+function onDestroy()
+{
+   //update post
+}
+
+function onStartCountdown()
+{
+   //starting
+}
+
+function onEndSong()
+{
+   //end song
+}
+
+//and that's all for now, I'll finish it in the next updates
